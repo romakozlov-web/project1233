2edd4485e918e8ba2e0cc9d29e6f0dc1d53127d7
Роман <roma.kozlov.2005@gmail.com>
2026-03-02 14:49:53 +0700

    удалено

--- Interface_operator_cinema/get_current_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'config.php';
-header('Content-Type: application/json');
-
-echo json_encode(['db' => DEFAULT_DB]);
--- Interface_operator_cinema/set_theme.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (isset($_GET['theme'])) {
-    $_SESSION['theme'] = $_GET['theme'];
-}
-?>
